6d2d375f379ce5cbb9f144dd791ab3224768eb9e
Eric Liang <etlgfx@gmail.com>
2017-01-29 13:47:32 -0500

    adding hello world

---
+++ src/main/java/com/viafoura/vfu/github/flow/Hello.java
@@ -0,0 +1,10 @@
+package com.viafoura.vfu.github.flow;
+
+/**
+ * Created by eric on 1/29/17.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Github Flow");
+    }
+}
